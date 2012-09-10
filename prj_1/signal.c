15e31bee733517bf5768367cc96b20227d8a06b1
mangesh.thakare <mangesh.thakare@gmail.com>
2012-09-10 13:37:47 +0000

    Program runs with small commands to, added check for number of strings in cmd

--- prj_1/signal.c
+++ prj_1/signal.c
@@ -12,6 +12,7 @@
 #include <signal.h>
 #include <setjmp.h>
 
+static int debug_en;
 // GLOBAL VARIABLES
 jmp_buf env;
 int state;
@@ -49,8 +50,37 @@ void signal_handler( int signal )
       {
          if ( ch != '\n')
             while ( (ch = getchar()) != '\n');
-         //longjmp (env, state);
+         longjmp (env, state);
       }
    }
    
 }
+
+
+
+//#include "signal.c"
+/*
+
+int main () 
+{
+  int i =0;
+
+  //Register signal and signal_handler. 
+  //This piece of code should be copied in the main function. 
+  //signal (SIGINT, signal_handler);
+  //state = setjmp ( env );
+  register_signal();
+
+
+  // Here goes the normal pgm, Below is just a Test sample pgm
+  while ( 1 )
+  {
+     printf ( " Signal handle test %d\n", i);
+     sleep (1);
+     i++;
+  }
+  return 1;
+
+}
+
+*/
