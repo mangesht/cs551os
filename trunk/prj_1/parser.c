9c599ea46ae549c6b0e587af9b961aed2a57ad88
rbaskar@hawk.iit.edu <rbaskar@hawk.iit.edu@81106e01-aae8-c3b9-e4fa-5397f91db96c>
2012-09-16 14:36:46 +0000

    raj checking in

--- trunk/prj_1/parser.c
+++ trunk/prj_1/parser.c
@@ -1,3 +1,12 @@
+//-------------------------------------------------------------
+// Illinois Institute of Technology, Chicago
+// CS 551 Implementation of Operating Systems
+// Fall 2012,  Group 3
+//
+// --- Description --------------------------------------------
+// Function module to handle command parsing
+//-------------------------------------------------------------
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -5,49 +14,25 @@
 #define PARAM_SIZE 30
 
 
-int parsecmd( char *, char ***);
-void handleException(int );
+int parsecmd( char *, char **);
+void isMemAllocated(char *mem );
 void cleanCmd(char **, int);
 char * substr(char *, int , int );
+void addCmdParam(char**,char*, char*,char*,int *);
 
-void cleanCmd(char **input_str, int n){
-    size_t i;
-    char *pCmd;
-    pCmd = *input_str;
-    char *p ;
-    // = pCmd;
-    //printf("cleanCmd : Instring %s len=%d\n",pCmd,n); 
-	for(i = 0; i < n; i++)
-	    if('\t' == pCmd[i])  pCmd[i] = ' ';
+/* Funtion: Parse Commands
+ * Returns parsed command in an array 
+*/
 
-    /* remove trailing space */
-    for(i = n-1; i >= 0; i--)
-        if(' ' == pCmd[i])   pCmd[i] = '\0';
-        else  break;
-
-    //printf("cleanCmd End removal : Instring %s len=%d\n",pCmd,n); 
-    /* remove leading space */
-    p=pCmd;
-    for(i = 0; i < n; i++)
-        if(pCmd[i] == 0x20 ) p = pCmd + i + 1;
-        else {
- 		    pCmd=p;
-            break;
-        }
-    *input_str = pCmd;
-	return;
-}
-
-int parseCmd(char * in_cmd, char *** cmd_list){
+int parseCmd(char * in_cmd, char ** cmd_list){
 
   int i,j=0,char_count=0, space_encountered=0;
-  char *cmd ;
+
   // handles cleaning the input command
-  cmd = (char *) malloc(256);
+  char * cmd = (char *) malloc(256);
   strcpy(cmd,in_cmd);
   cleanCmd(&cmd, strlen(cmd));
-  //printf("cleanCmd : Outstring %s\n",cmd); 
-  char ** cmd_params = *cmd_list;
+
   // hanldes alias command
   if ( strstr(cmd,"alias ") || strstr(cmd,"set ")){ 
 	 
@@ -59,7 +44,7 @@ int parseCmd(char * in_cmd, char *** cmd_list){
 	if (ptr != NULL && ptr==cmd ){
 	   char * pch = (char*)strtok (ptr," =");
 	   while (pch != NULL){	  
-		  cmd_params[j++]= pch;
+		  cmd_list[j++]= pch;
 		  pch = strtok (NULL, "=");
 
 		  if (pch!=NULL){
@@ -72,26 +57,37 @@ int parseCmd(char * in_cmd, char *** cmd_list){
 				}
 		   }
 		}
+	 cmd_list[j]=NULL;
 	 return 1;	
 	 }
   }
 
 
-  char * mem =  (char*)malloc(sizeof(char)* 100);
-  if (mem == NULL )  handleException(1);
+  char *mem, *pLoc = NULL, * temp= NULL;
+  isMemAllocated( mem =  (char*)malloc(sizeof(char)* 100));
   char * param = mem;
 
-  char *pLoc;
+
   for (i=0;i<strlen(cmd)+1;i++){
+   	  if(pLoc!=NULL){
+	     if( pLoc-param>0)
+         cmd_list[j++]=  substr(param,0, pLoc-param);
+   	     cmd_list[j++]= temp;
+	     mem =  (char*)malloc(sizeof(char)* PARAM_SIZE);
+	     param=mem;
+	     pLoc=NULL;
+	    }
+
+
    if ( (cmd[i]=='&' || cmd[i]=='>' || cmd[i]=='|' || cmd[i]=='\0' )&& char_count != 0 ){
    	   *mem='\0';
-	   cmd_params[j++]= param;
+	   cmd_list[j++]= param;
 	   if (cmd[i] != '\0'){
 	       mem =  (char*)malloc(sizeof(char)* 2);
 	       param=mem;
 	       *mem++ = cmd[i];
-		   *mem = '\0';
-	       cmd_params[j++]= param;
+	       *mem = '\0';
+	       cmd_list[j++]= param;
 
 	       mem =  (char*)malloc(sizeof(char)* PARAM_SIZE);
 	       param=mem;
@@ -111,72 +107,84 @@ int parseCmd(char * in_cmd, char *** cmd_list){
 		char_count++;
         *mem++ = cmd[i];
         *mem='\0';
-        
+      
+
       pLoc=strstr(param,"if ");
-      if( pLoc!=NULL) {	  	  
-	      char * temp = (char*)malloc(sizeof(char)* 3);
-	  	  temp="if";	  
-		  cmd_params[j++]= substr(param,0, pLoc-param);
-  		  cmd_params[j++]= temp;	
-	 	  mem =  (char*)malloc(sizeof(char)* PARAM_SIZE);
-	      param=mem;			
+      if( pLoc!=NULL) {	 
+		  isMemAllocated(temp = (char*)malloc(sizeof(char)* 3));		  
+	  	  temp="if"; continue;
 	  }
 	  
 	  pLoc=strstr(param," then ");
-      if( pLoc!=NULL) {
-	  	  char * temp = (char*)malloc(sizeof(char)* 7);
-	  	  temp="then";
-	 	  cmd_params[j++]= substr(param,0, pLoc-param);	  
-		  cmd_params[j++]= temp;
-	 	  mem =  (char*)malloc(sizeof(char)* PARAM_SIZE);
-	      param=mem;
+      if( pLoc!=NULL) {  
+	  	 isMemAllocated(temp = (char*)malloc(sizeof(char)* 5));
+	  	  temp="then"; continue;
 	  }
 	  
-
 	  
 	  pLoc=strstr(param," else ");
-      if( pLoc!=NULL) {
-	      char * temp = (char*)malloc(sizeof(char)* 7);
-	  	  temp="else";	  
-	  	  cmd_params[j++]=  substr(param,0, pLoc-param);
-  	      cmd_params[j++]= temp;
-	 	  mem =  (char*)malloc(sizeof(char)* PARAM_SIZE);
-	      param=mem;
+      if( pLoc!=NULL) {	   	  
+	    isMemAllocated(temp = (char*)malloc(sizeof(char)* 5));
+	  	  temp="else"; continue;
 	  }
 	  
 	  pLoc=strstr(param," fi");
-      if( pLoc!=NULL) {
-	  	  char * temp = (char*)malloc(sizeof(char)* 3);
-	  	  temp="fi";	  
-	 	  cmd_params[j++]=  substr(param,0, pLoc-param);
-   	      cmd_params[j++]= temp;
-	 	  mem =  (char*)malloc(sizeof(char)* PARAM_SIZE);
-	      param=mem;	    
-	  }
+      // if( pLoc!=NULL && (*(pLoc+3) !=' ' )) { 
+			if( pLoc!=NULL){
+	  	  isMemAllocated(temp = (char*)malloc(sizeof(char)* 3));
+	  	  temp="fi"; continue;
+	  }		
 	  }
    }
-   for(i=0;cmd_params[i] != NULL ; i++) {
+   for(i=0;cmd_list[i] != NULL ; i++) {
         //printf("Calling clearn on %d \n",i);
-        cleanCmd(&cmd_params[i],strlen(cmd_params[i]));
+        cleanCmd(&cmd_list[i],strlen(cmd_list[i]));
    }
 return 1 ;
 }
 
-
-void handleException(int code){
-  switch (code){
- 	case 1 : printf(" Out of Memory !!!");
-    	      exit(0);
-  }
+/* Function: Checks and throws error if memory is not allocated properly			
+ */
+void isMemAllocated( char *mem){
+   if (mem == NULL){
+	printf(" Out of Memory !!!");
+    exit(0);
+   }
 }
 
+/* Function: Cleans the input command for tabs, leading and trailing spaces							
+ */
+void cleanCmd(char **input_str, int n){
+    size_t i;
+    char *pCmd;
+    pCmd = *input_str;
+    char *p ;
 
+	for(i = 0; i < n; i++)
+	    if('\t' == pCmd[i])  pCmd[i] = ' ';
 
+    /* remove trailing space */
+    for(i = n-1; i >= 0; i--)
+        if(' ' == pCmd[i])   pCmd[i] = '\0';
+        else  break;
 
-/* Extracts substring from the given string 								*/
-/* Input:  	 pstr - source string											*/
-/*  		 start - start location  string									*/
-/*  		 numchars - no of characters to extract from start location		*/
+    /* remove leading space */
+    p=pCmd;
+    for(i = 0; i < n; i++)
+        if(pCmd[i] == 0x20 ) p = pCmd + i + 1;
+        else {
+ 		    pCmd=p;
+            break;
+        }
+    *input_str = pCmd;
+	return;
+}
+
+/* Function: Extracts substring from the given string 								
+ * Input:  	 pstr - source string											
+ *  		 start - start location  string									
+ *  		 numchars - no of characters to extract from start location		
+ */
 char * substr(char *pstr, int start, int numchars){
 	char *pnew = malloc(numchars+1);
 	strncpy(pnew, pstr + start, numchars);
