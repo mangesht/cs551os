7c5d67f857225b627540fa40bc02e8a985bc73b6
mangesh.thakare <mangesh.thakare@gmail.com>
2012-10-20 18:25:52 +0000

    added mailbox.c main file

---
+++ prj_2/src/mailbox.c
@@ -0,0 +1,74 @@
+#include "fs.h"
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include<stdio.h>
+#include<errno.h>
+#include<stdarg.h>
+#include <string.h>
+#include <minix/callnr.h>
+#include <minix/safecopies.h>
+#include <minix/endpoint.h>
+#include <minix/com.h>
+#include <minix/sysinfo.h>
+#include <minix/u64.h>
+#include <sys/ptrace.h>
+#include <sys/svrctl.h>
+#include "file.h"
+#include "fproc.h"
+#include "scratchpad.h"
+#include "dmap.h"
+#include <minix/vfsif.h>
+#include "vnode.h"
+#include "vmnt.h"
+#include "param.h"
+
+
+FILE *logFile = NULL;
+
+void lprint(char * format, ...)
+{
+    va_list args;
+    if(!logFile){
+        logFile = fopen("/usr/log.ini","w");
+    }
+    va_start(args,format);
+    vfprintf(logFile,format,args);
+    fflush(logFile);
+    va_end(args);
+
+}
+
+
+PUBLIC int do_deposit()
+{
+   lprint("Do deposit called \n");
+   return 0;
+}
+
+PUBLIC int do_retrieve() 
+{
+    return 0;
+}
+PUBLIC int do_destroy_mailbox() 
+{
+    return 0;
+}
+PUBLIC int do_create_mailbox()
+{
+    return 0;
+}
+PUBLIC int do_get_av_mailboxes()
+{
+    return 0;
+}
+PUBLIC int do_register() 
+{
+    return 0;
+}
+
+PUBLIC int do_get_senders()
+{
+    return 0;
+}
+
