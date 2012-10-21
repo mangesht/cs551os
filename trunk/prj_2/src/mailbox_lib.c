d9c2f04aee152109393fa09d69a1987e339c6ed4
mangesh.thakare@gmail.com <mangesh.thakare@gmail.com@81106e01-aae8-c3b9-e4fa-5397f91db96c>
2012-10-21 07:52:55 +0000

    Updated code 3 functions done

--- trunk/prj_2/src/mailbox_lib.c
+++ trunk/prj_2/src/mailbox_lib.c
@@ -24,8 +24,9 @@ PUBLIC int get_senders(int *sender_list);
 // It usage in our project
 
 // {int m7i1, m7i2, m7i3, m7i4, m7i5; char *m7p1, *m7p2;} mess_7;
-// m7i1    -> Calling process User Id 
-// m7i2    -> Calling process Group Id
+// m7i1    -> Calling process Id PID 
+// m7i2    -> Calling process User Id 
+// m7i3    -> Calling process Group Id
 // m7p1    -> String message 
 // m7p2    -> List of destinations for this message 
 
@@ -34,43 +35,44 @@ PUBLIC int get_senders(int *sender_list);
 
 PUBLIC int deposit(int *dst, char *msg){
     message mes;
- //   mes.m7i1 = getuid();
- //   mes.m7i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
+ //   mes.m7i2 = getuid();
+ //   mes.m7i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
     mes.m7_p1 = msg; 
     mes.m7_p2 = dst;
+    printf("Process %d sending message = %s to ",mes.m7_i1,msg);
     return ( (int) _syscall(VFS_PROC_NR,DEPOSIT,&mes));
   // For referece  return( (uid_t) _syscall(VFS_PROC_NR, GETUID, &m));
 }
 PUBLIC int retrieve(int source , char *msg) 
 {
     message mes;
-//    mes.m7i1 = getuid();
-//    mes.m7i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
+//    mes.m7i2 = getuid();
+//    mes.m7i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
     mes.m7_p1  = msg;
     return ( (int) _syscall(VFS_PROC_NR,RETRIEVE,&mes));
 }
 PUBLIC int create_mailbox(int permissions)
 {
     message mes;
-    mes.m7_i1 = getuid();
-    mes.m7_i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
-    mes.m7_i3 = permissions;
+    mes.m7_i2 = getuid();
+    mes.m7_i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
+    mes.m7_i4 = permissions;
     return ( (int) _syscall(VFS_PROC_NR,CREATE_MB,&mes));
 }
 
 PUBLIC int destroy_mailbox(int id) 
 {
     message mes;
-//    mes.m7_i1 = getuid();
-//    mes.m7_i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
+//    mes.m7_i2 = getuid();
+//    mes.m7_i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
     mes.m7_i3 = id;
     return ( (int) _syscall(VFS_PROC_NR,DESTROY_MB,&mes));
 
@@ -78,10 +80,10 @@ PUBLIC int destroy_mailbox(int id)
 PUBLIC int get_av_mailboxes(int *mb_list)
 {
     message mes;
-    mes.m7_i1 = getuid();
-    mes.m7_i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
+    mes.m7_i2 = getuid();
+    mes.m7_i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
     mes.m7_p1 = mb_list;
     return ( (int) _syscall(VFS_PROC_NR,GET_AV_MB,&mes));
 }
@@ -89,20 +91,20 @@ PUBLIC int get_av_mailboxes(int *mb_list)
 PUBLIC int register_mb(int mb_id)
 {
     message mes;
-    mes.m7_i1 = getuid();
-    mes.m7_i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
-    mes.m7_i3 = mb_id;
+    mes.m7_i2 = getuid();
+    mes.m7_i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
+    mes.m7_i4 = mb_id;
     return ( (int) _syscall(VFS_PROC_NR,REGISTER,&mes));
 }
 PUBLIC int get_senders(int *sender_list)
 {
     message mes;
-//    mes.m7_i1 = getuid();
-//    mes.m7_i2 = getgid();
-    mes.m_source = getpid();
-    mes.m_type = 7 ;
+//    mes.m7_i2 = getuid();
+//    mes.m7_i3 = getgid();
+    mes.m7_i1 = getpid();
+//    mes.m_type = 7 ;
     mes.m7_p1 = sender_list;
     return ( (int) _syscall(VFS_PROC_NR,GET_SENDERS,&mes));
 }
