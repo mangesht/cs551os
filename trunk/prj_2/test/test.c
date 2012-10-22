9c905f558309dd41b6053d619ab712428d6217bd
pushpadevi.c@gmail.com <pushpadevi.c@gmail.com@81106e01-aae8-c3b9-e4fa-5397f91db96c>
2012-10-22 17:38:24 +0000

    initial version

---
+++ trunk/prj_2/test/test.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include <string.h>
+
+int test_suite1(){
+   int res, res1, res2, res3, retVal, test_id = 1 ;
+   int * mb_list = NULL;
+   int i = 0;
+   printf ( " ---------Test %d: Creating mail box with Self permission--------\n", test_id);
+   res = create_mailbox(0x4);
+   
+   if(res == -1){
+	   printf( "SYSTEM CALL FAIL: Failed to create mail box with self permission \n" );
+   }else {
+          printf("Success: create mail box with self permission returned  %d ",res);
+   }
+   
+   test_id++;
+   printf ( "\n\n ------ Test  %d: Creating mail box with Group permission------\n", test_id);
+   res1 = create_mailbox(0x2);
+    if(res1 == -1){
+	   printf( "SYSTEM CALL FAIL: Failed to create mail box with group permission \n" );
+   }else {
+          printf("Success: create mail box with group permission returned  %d ",res1);
+   }
+   
+   test_id++;
+   printf ( "\n\n-----Test  %d: Creating mail box with everyone permission------\n", test_id);
+   res2 = create_mailbox(0x1);
+  if(res2 == -1){
+	   printf( "SYSTEM CALL FAIL: Failed to create mail box with everyone permission \n" );
+   }else {
+          printf("Success: create mail box with everyone permission returned  %d ",res2);
+   }
+   
+   test_id++;
+   printf ( "\n\n------Test  %d: Creating mail box with different permission   ------\n", test_id);
+   res3 = create_mailbox(0x9);
+  if(res3 == -1){
+	   printf( "SYSTEM CALL FAIL: Failed to create mail box with everyone permission \n" );
+   }else {
+          printf("Success: create mail box with everyone permission returned  %d ",res3);
+   }
+   
+   // THROWING AN ERROR : kernel: /usr/src/kernel/system/do_coopy.c:70: bad endpoint 561: proc 583 out of range
+   // do_copy :1 : seg 0x1, 561 not ok endpoint 
+
+   test_id++;
+   printf ( "\n\n------ Test  %d: get_av_mailboxes------\n", test_id);
+   mb_list = (int * ) malloc (10 *2*sizeof(int));
+   retVal = get_av_mailboxes(&mb_list);
+  if(retVal == -1){
+	   printf( "SYSTEM CALL FAIL: get_av_mailboxes FAILED \n" );
+   }else {
+          printf("Success: get_av_mailboxes retVal =  %d\n", retVal );
+	//   i = 0;
+	/*   while  ( *mb_list != NULL) {
+	      //for ( i = 0 ; i < 20 ; i = i+2 ) {
+		   printf("mb_ %d _id  = %d \t mb_%d_procID = %d\n", i, *mb_list, i, *(mb_list +1) );
+		   i = i+2; 
+           }
+*/	   
+   }
+    
+   
+   test_id++;
+   printf ( "\n\n------ Test  %d: Register mailbox VALID case------\n", test_id);
+      res = register_mb(res);
+      if(res == -1){
+          printf( "SYSTEM CALL FAIL: Failed to register mail box\n" );
+     }else if(res == 1){
+	   printf("Success: register  mail box  returned  %d ",res);
+     }else if(res == 2){
+	   printf("Success: Sender already Registered %d ",res);
+     }
+     
+   
+test_id++;
+   printf ( "\n\n------ Test  %d: Register mailbox INVALID case------\n", test_id);
+      res = register_mb(10);
+     if(res == -1){
+          printf( "Success: Failed to register mail box\n" );
+     }else {
+	   printf("FAIL: Register mailbox returned success");
+     }
+
+   
+     test_id++;
+      printf ( "\n\n------Test  %d: Authorized Delete  mail box of Self permission ------\n", test_id);
+      res = destroy_mailbox(res);
+      if(res == -1){
+          printf( "SYSTEM CALL FAIL: Failed to delete mail box\n" );
+     }else {
+	   printf("Success: delete mail box  returned  %d ",res);
+     }   
+     
+     test_id++;
+      printf ( "\n\n------Test  %d: Authorized Delete  mail box of group permission------\n", test_id);
+      res = destroy_mailbox(res1);
+      if(res == -1){
+          printf( "SYSTEM CALL FAIL: Failed to delete mail box\n" );
+     }else {
+	   printf("Success: delete mail box  returned  %d ",res);
+     }   
+   
+   test_id++;
+    printf ( "\n\n------ Test  %d: Authorized Delete  mail box of Everyone permission------\n", test_id);
+      res = destroy_mailbox(res2);
+      if(res == -1){
+          printf( "SYSTEM CALL FAIL: Failed to delete mail box\n" );
+     }else {
+	   printf("Success: delete mail box  returned  %d ",res);
+     }   
+   
+   
+   
+   test_id++;
+    printf ( "\n\n------ Test  %d: Authorized Delete  mail box of other permission------\n", test_id);
+      res = destroy_mailbox(res3);
+      if(res == -1){
+          printf( "SYSTEM CALL FAIL: Failed to delete mail box\n" );
+     }else {
+	   printf("Success: delete mail box  returned  %d ",res);
+     }   
+     
+   
+   //Test 7: Tested mailbox full also by executing the same exe 3 times which says  Error: mail box space full
+   // Test 8: UnAuthorized delete tested 
+}
+
+
+int main(){
+int res;
+
+test_suite1();
+//test_delete_mb();
+
+return 0;
+}
