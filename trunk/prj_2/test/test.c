40cc1875f836d13805a9c7994aad7c5d7277dd73
pushpadevi.c@gmail.com <pushpadevi.c@gmail.com@81106e01-aae8-c3b9-e4fa-5397f91db96c>
2012-10-23 09:04:30 +0000

    added TC

--- trunk/prj_2/test/test.c
+++ trunk/prj_2/test/test.c
@@ -7,6 +7,7 @@ int test_suite1(){
    int * sender_list = NULL;
    int i = 0;
    
+   
    printf ( " ---------Test %d: Creating mail box with Self permission--------\n", test_id);
    res = create_mailbox(0x4);
       if(res == -1){
@@ -14,6 +15,8 @@ int test_suite1(){
    }else {
           printf("Success: create mail box with self permission returned  %d ",res);
    }
+
+
    
    test_id++;
    printf ( "\n\n ------ Test  %d: Creating mail box with Group permission------\n", test_id);
@@ -24,7 +27,9 @@ int test_suite1(){
           printf("Success: create mail box with group permission returned  %d ",res1);
    }
    
-   test_id++;
+  
+
+  test_id++;
    printf ( "\n\n-----Test  %d: Creating mail box with everyone permission------\n", test_id);
    res2 = create_mailbox(0x1);
   if(res2 == -1){
@@ -33,9 +38,11 @@ int test_suite1(){
           printf("Success: create mail box with everyone permission returned  %d ",res2);
    }
    
+
+
    test_id++;
    printf ( "\n\n------Test  %d: Creating mail box with different permission   ------\n", test_id);
-   res3 = create_mailbox(0x9);
+   res3 = create_mailbox(0x8);
   if(res3 == -1){
 	   printf( "SYSTEM CALL FAIL: Failed to create mail box with everyone permission \n" );
    }else {
@@ -46,6 +53,7 @@ int test_suite1(){
    test_id++;
    printf ( "\n\n------ Test  %d: get_av_mailboxes------\n", test_id);
    mb_list = (int * ) malloc (10 *2*sizeof(int));
+   //mb_list[30];
    retVal = get_av_mailboxes(mb_list);
   if(retVal == -1){
 	   printf( "SYSTEM CALL FAIL: get_av_mailboxes FAILED \n" );
@@ -71,7 +79,22 @@ int test_suite1(){
 	   printf("Success: Sender already Registered \n");
      }
      
-   test_id++;
+
+     
+          test_id++;
+   printf ( "\n\n------ Test  %d: Register mailbox VALID case------\n", test_id);
+      retVal = register_mb(res1);
+      printf ( "%d", retVal);
+      if(retVal == -1){
+          printf( "FAIL: Failed to register mail box\n" );
+     }else if(retVal == 0){
+	   printf("Success: register  mail box  returned \n");
+     }else if(retVal == -2){
+	   printf("Success: Sender already Registered \n");
+     }
+     
+
+     test_id++;
    printf ( "\n\n------ Test  %d: Register mailbox Sending already exist------\n", test_id);
       retVal = register_mb(res);
       printf ( "%d", retVal);
@@ -84,9 +107,10 @@ int test_suite1(){
      }
    
    
-test_id++;
+
+  test_id++;
    printf ( "\n\n------ Test  %d: Register mailbox INVALID case------\n", test_id);
-      retVal = register_mb(10);
+      retVal = register_mb(8);
      if(retVal == -1){
           printf( "Success: Failed to register mail box\n" );
      }else {
@@ -95,39 +119,36 @@ test_id++;
 
    
    
-   
 test_id++;
    printf ( "\n\n------ Test  %d: get Senders ------\n", test_id);
    sender_list = (int * ) malloc (10 *sizeof(int));   
-   retVal = get_senders(res, sender_list);
+   retVal = get_senders(res1, sender_list);
    if(retVal == -1){
 	   printf( "FAIL: get Senders  FAILED \n" );
    }else {
           printf("Success: get_senders retVal =  %d\n", retVal );
 	 for ( i = 0 ; i < 10 ; i ++) {
-		   printf("mb_%d_procID = %d\n",  i, *(mb_list + i) );
-		    
+		   printf("sender_%d_procID = %d\n",  i, *(sender_list + i) );
            }
-  
-   }
+     }
    free (sender_list);
-   
    
    
    
 test_id++;
    printf ( "\n\n------ Test  %d: get Senders -INVALID-----\n", test_id);
-   sender_list = (int * ) malloc (10 *sizeof(int));   
-   retVal = get_senders(9, sender_list);
+   sender_list = (int * ) malloc (10*sizeof(int));   
+   retVal = get_senders(8, sender_list);
    if(retVal == -1){
 	   printf( "SUCESS: get Senders  FAILED \n" );
    }else {
           printf("FAIL: get_senders retVal =  %d\n", retVal );
 	 for ( i = 0 ; i < 10 ; i ++) {
-		   printf("mb_%d_procID = %d\n",  i, *(mb_list + i) );
+		   printf("sender_%d_procID = %d\n",  i, *(sender_list + i) );
            }
    }
    free (sender_list);
+
    
    
      test_id++;
@@ -138,7 +159,9 @@ test_id++;
      }else {
 	   printf("Success: delete mail box  returned  %d ",retVal);
      }   
-     
+   
+
+   
      test_id++;
       printf ( "\n\n------Test  %d: Authorized Delete  mail box of group permission------\n", test_id);
       retVal = destroy_mailbox(res1);
@@ -147,7 +170,9 @@ test_id++;
      }else {
 	   printf("Success: delete mail box  returned  %d ",res);
      }   
-   
+
+
+     
    test_id++;
     printf ( "\n\n------ Test  %d: Authorized Delete  mail box of Everyone permission------\n", test_id);
       retVal = destroy_mailbox(res2);
