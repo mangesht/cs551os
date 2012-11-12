2c24c4ce0f3e1c81158cf82f9596782918f9c848
rbaskar@hawk.iit.edu <rbaskar@hawk.iit.edu@81106e01-aae8-c3b9-e4fa-5397f91db96c>
2012-11-12 13:36:57 +0000

    raj

--- trunk/prj_3/src/fs_tool_lib.c
+++ trunk/prj_3/src/fs_tool_lib.c
@@ -12,3 +12,13 @@ PUBLIC int fileinfo(char *fname,int mode ) {
 //  printf("In Lib : Getting info for file %s Mode = %d \n",m.m1_p1,m.m1_i2); 
   return(_syscall(VFS_PROC_NR,FILEINFO,&m));
 }
+
+
+PUBLIC int fraginfo(char *fname,int mode ) {
+  message m;
+  m.m1_i1 = strlen(fname) + 1 ; 
+  m.m1_p1 = fname ;
+  m.m1_i2 = mode;
+//  printf("In Lib : Getting fragmentation info for file %s Mode = %d \n",m.m1_p1,m.m1_i2); 
+  return(_syscall(VFS_PROC_NR,FRAGINFO,&m));
+}
